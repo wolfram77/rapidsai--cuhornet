7da5f96d62d842981edb301b582601d26b0f612c
Subhajit Sahu <wolfram77@gmail.com>
2023-10-31 23:41:05 +0530

    :bug: run on all graphs

--- main.sh
+++ main.sh
@@ -15,7 +15,7 @@ if [[ "$DOWNLOAD" != "0" ]]; then
 fi
 cd $src
 
-# Build and run
+# Build
 export RMM_INCLUDE=$PWD/externals/rmm/include
 cd hornet/build
 cmake -DRMM_INCLUDE=$RMM_INCLUDE ..
@@ -24,3 +24,24 @@ cd ../..
 cd hornetsnest/build
 cmake -DRMM_INCLUDE=$RMM_INCLUDE ..
 make -j32
+
+# Run on all graphs.
+perform-all() {
+stdbuf --output=L ./katz ~/Data/indochina-2004.mtx  2>&1 | tee -a "$out"
+stdbuf --output=L ./katz ~/Data/arabic-2005.mtx     2>&1 | tee -a "$out"
+stdbuf --output=L ./katz ~/Data/uk-2005.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./katz ~/Data/webbase-2001.mtx    2>&1 | tee -a "$out"
+stdbuf --output=L ./katz ~/Data/it-2004.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./katz ~/Data/sk-2005.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./katz ~/Data/com-LiveJournal.mtx 2>&1 | tee -a "$out"
+stdbuf --output=L ./katz ~/Data/com-Orkut.mtx       2>&1 | tee -a "$out"
+stdbuf --output=L ./katz ~/Data/asia_osm.mtx        2>&1 | tee -a "$out"
+stdbuf --output=L ./katz ~/Data/europe_osm.mtx      2>&1 | tee -a "$out"
+stdbuf --output=L ./katz ~/Data/kmer_A2a.mtx        2>&1 | tee -a "$out"
+stdbuf --output=L ./katz ~/Data/kmer_V1r.mtx        2>&1 | tee -a "$out"
+}
+perform-all
+perform-all
+perform-all
+perform-all
+perform-all
